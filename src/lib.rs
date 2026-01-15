53909eb93b93127445e6f4c9549c8b0c13fc0600
Wisdom Ogwu <iammadab@gmail.com>
2026-01-15 11:50:31 +0100

    define the memory emulator trait

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,15 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+trait MemoryEmulator {
+    fn load_u8(&self, addr: u64) -> u8;
+    fn load_u16(&self, addr: u64) -> u16;
+    fn load_u32(&self, addr: u64) -> u32;
+    fn load_u64(&self, addr: u64) -> u64;
+
+    fn store_u8(&mut self, addr: u64, value: u8);
+    fn store_u16(&mut self, addr: u64, value: u16);
+    fn store_u32(&mut self, addr: u64, value: u32);
+    fn store_u64(&mut self, addr: u64, value: u64);
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+fn test_memory_emulator<M: MemoryEmulator>(mem: M) {
+    todo!()
 }
