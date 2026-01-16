1e5fa383eef00bd2da0ae30b107e3729a3fee405
Wisdom Ogwu <iammadab@gmail.com>
2026-01-16 07:02:37 +0100

    take emulator as mut

--- src/main.rs
+++ src/main.rs
@@ -10,42 +10,44 @@ use fast_mem::emulators::paged_last_cache::{
 use fast_mem::replay_mem_operations;
 
 fn main() {
-    bench_fib(PagedMemoryDefault::default());
-    bench_fib(PagedMemoryAHash::default());
-    bench_fib(PagedMemoryFxHash::default());
-    bench_fib(PagedMemoryNoHashU64::default());
+    // bench_fib(PagedMemoryDefault::default());
+    // bench_fib(PagedMemoryAHash::default());
+    // bench_fib(PagedMemoryFxHash::default());
+    // bench_fib(PagedMemoryNoHashU64::default());
+    //
+    // bench_exec_block(PagedMemoryDefault::default());
+    // bench_exec_block(PagedMemoryAHash::default());
+    // bench_exec_block(PagedMemoryFxHash::default());
+    // bench_exec_block(PagedMemoryNoHashU64::default());
+    //
+    // bench_fib(PagedMemoryCacheLastDefault::default());
+    // bench_fib(PagedMemoryCacheLastAHash::default());
+    // bench_fib(PagedMemoryCacheLastFxHash::default());
+    // bench_fib(PagedMemoryCacheLastNoHashU64::default());
+    //
+    // bench_exec_block(PagedMemoryCacheLastDefault::default());
+    // bench_exec_block(PagedMemoryCacheLastAHash::default());
+    // bench_exec_block(PagedMemoryCacheLastFxHash::default());
+    // bench_exec_block(PagedMemoryCacheLastNoHashU64::default());
 
-    bench_exec_block(PagedMemoryDefault::default());
-    bench_exec_block(PagedMemoryAHash::default());
-    bench_exec_block(PagedMemoryFxHash::default());
-    bench_exec_block(PagedMemoryNoHashU64::default());
-
-    bench_fib(PagedMemoryCacheLastDefault::default());
-    bench_fib(PagedMemoryCacheLastAHash::default());
-    bench_fib(PagedMemoryCacheLastFxHash::default());
-    bench_fib(PagedMemoryCacheLastNoHashU64::default());
-
-    bench_exec_block(PagedMemoryCacheLastDefault::default());
-    bench_exec_block(PagedMemoryCacheLastAHash::default());
-    bench_exec_block(PagedMemoryCacheLastFxHash::default());
-    bench_exec_block(PagedMemoryCacheLastNoHashU64::default());
+    bench_exec_block(&mut PagedMemoryCacheLastFxHash::default());
 }
 
-fn bench_exec_block<M: MemoryEmulator>(emulator: M) {
+fn bench_exec_block<M: MemoryEmulator>(emulator: &mut M) {
     let label = format!("{}: exec_block", emulator.name());
     bench_memory_replay(label, "mem_bin/mem-exec-block-gc.bin", emulator);
 }
 
-fn bench_fib<M: MemoryEmulator>(emulator: M) {
+fn bench_fib<M: MemoryEmulator>(emulator: &mut M) {
     let label = format!("{}: fib", emulator.name());
     bench_memory_replay(label, "mem_bin/mem-fib-gc.bin", emulator);
 }
 
 /// Time a memory emulator against a replay file
-fn bench_memory_replay<M: MemoryEmulator>(label: String, path: &'static str, mut emulator: M) {
+fn bench_memory_replay<M: MemoryEmulator>(label: String, path: &'static str, emulator: &mut M) {
     let start = std::time::Instant::now();
     println!("{}", label);
-    replay_mem_operations(path, &mut emulator);
+    replay_mem_operations(path, emulator);
     let duration = start.elapsed();
     println!("{:?}", duration);
 }
