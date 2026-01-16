048a9c3d7298fe77fe2d432d440599eb0c7ad37c
Wisdom Ogwu <iammadab@gmail.com>
2026-01-16 05:13:23 +0100

    full run

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,18 @@ use fast_mem::emulators::noop::NoopMem;
 use fast_mem::emulators::paged::{
     PagedMemoryAHash, PagedMemoryDefault, PagedMemoryFxHash, PagedMemoryNoHashU64,
 };
+use fast_mem::emulators::paged_last_cache::PagedMemoryCacheLast;
 use fast_mem::replay_mem_operations;
 
 fn main() {
     bench_fib("Noop: Fib", NoopMem::default());
     bench_exec_block("Noop: Exec Block", NoopMem::default());
+
+    bench_fib("PageMemoryLastCache: Fib", PagedMemoryCacheLast::default());
+    bench_exec_block(
+        "PageMemoryLastCache: Exec Block",
+        PagedMemoryCacheLast::default(),
+    );
 
     // bench_fib("(Fib) Paged Memory: FxHash", PagedMemoryFxHash::default());
     // bench_fib("(Fib) Paged Memory: Ahash", PagedMemoryAHash::default());
